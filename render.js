6274218de6850f87a716fb3001a9beeb9f0ff466
Quinn Dizon <qdizon@qdizon.fios-router.home>
2019-03-31 16:13:33 -0400

    fixes to request

--- render.js
+++ render.js
@@ -2,5 +2,6 @@ const queryJobs = require('./js/queryJobs');
 
 document.getElementById("test").onclick = (e) => {
     queryJobs("https://www.indeed.com/jobs?q=&l=washington+dc&fromage=last", 25);
+    // queryJobs("https://www.indeed.com/jobs?q=&l=washington+dc&fromage=last", 25);
 }
 
