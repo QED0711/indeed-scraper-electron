cf87c009246d8be8e123602ee1fcd04e6c0a44bc
Quinn Dizon <qdizon@qdizon.fios-router.home>
2019-03-31 16:36:23 -0400

    started work on form refactor

--- render.js
+++ render.js
@@ -1,6 +1,8 @@
 const queryJobs = require('./js/queryJobs');
 
-document.getElementById("test").onclick = (e) => {
+document.getElementById("search-filter-form").onsubmit = (e) => {
+    e.preventDefault();
+    // write function that returs object that has url and other user defined params
     queryJobs("https://www.indeed.com/jobs?q=&l=washington+dc&fromage=last", 25);
     // queryJobs("https://www.indeed.com/jobs?q=&l=washington+dc&fromage=last", 25);
 }
